ea1d6fbdc6328b1b4f5505d5285a0add4dc6eb98
emmanuel-yegon <kptooman@gmail.com>
2023-07-02 00:10:35 +0300

    java love

--- src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
+++ src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
@@ -13,6 +13,10 @@ public class ProductWrapper {
     Integer categoryId;
     String categoryName;
 
+    public ProductWrapper() {
+
+    }
+
     public ProductWrapper(Integer id, String name, String description, Integer price, String status, Integer categoryId, String categoryName) {
         this.id = id;
         this.name = name;
@@ -21,5 +25,17 @@ public class ProductWrapper {
         this.status = status;
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public ProductWrapper(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public ProductWrapper(Integer id, String name, String description, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
     }
 }
