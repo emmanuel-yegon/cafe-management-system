68e16577b733e0ad47a6fade4118476a73cc6281
emmanuel-yegon <kptooman@gmail.com>
2023-07-01 15:58:02 +0300

    spring boot api

--- src/main/java/com/inn/cafe/dao/CategoryDao.java
+++ src/main/java/com/inn/cafe/dao/CategoryDao.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryDao extends JpaRepository<Category, Integer> {
-
     List<Category> getAllCategory();
 }
